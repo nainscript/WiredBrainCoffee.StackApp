384de5d28b15a99436df758e3ff51cb8d0ee4ab1
nains <nains@DESKTOP-HRO7RQU>
2021-10-14 11:58:46 -0700

    Add project files.

---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack
+    {
+        private double[] items;
+        private int _currentIndex = -1;
+        public SimpleStack()
+        {
+            items = new double[10];
+        }
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(double item)
+        {
+            items[++_currentIndex] = item;
+        }
+
+        public double Pop()
+        {
+            return items[_currentIndex--];
+        }
+    }
+}
